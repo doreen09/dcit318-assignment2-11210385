bfc9945dc9b200e50f4f4dd09453c954965f713e
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-09 11:49:16 +0000

    created a class Car that implements IMovable

--- Imovable/Imovable.cs
+++ Imovable/Imovable.cs
@@ -5,3 +5,19 @@ interface IMovable
     void Move();
 }
 
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+class App
+{
+    static void Main()
+    {
+        Car car = new Car();
+        car.Move();
+
+    }
+}
