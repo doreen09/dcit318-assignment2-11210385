ecce8e43503ab88b40ed42e7f96e6bb909eb93a7
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-09 08:17:53 +0000

    Created a base class 'Make sound' which prints 'Some generic sound'

---
+++ Inheritance/Inheritance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        new Animal().MakeSound();
+    }
+}
+ 
