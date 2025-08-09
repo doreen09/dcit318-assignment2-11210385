92312259b7e28ed1c01fb27d304e27f35fe54af0
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-09 10:28:14 +0000

    Created a derived class Rectangle that implements GetArea() to calculate the area of a rectangle

--- AbstractMethods/AbstractMethods.cs
+++ AbstractMethods/AbstractMethods.cs
@@ -13,13 +13,22 @@ class Circle : Shape
     public override double GetArea() => 3.14 * r * r;
 }
 
+class Rectangle : Shape
+{
+
+    double w, h;
+    public Rectangle(double width, double height) { w = width; h = height; }
+    public override double GetArea() => w * h;
+}
 
 class App
 {
     static void Main()
     {
         Shape c = new Circle(5);
-        Console.WriteLine("Circle Area: " + c.GetArea());
+        Shape r = new Rectangle(4, 6);
 
+        Console.WriteLine("Circle Area: " + c.GetArea());
+        Console.WriteLine("Rectangle Area: " + r.GetArea());
     }
 }
