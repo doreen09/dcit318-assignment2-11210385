0b8ccf42fd41cc1e26c5a127e968719cb3f6c549
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-09 10:15:37 +0000

    Created a derived class Circle that implements GetArea() to calculate the area of a circle

--- AbstractMethods/AbstractMethods.cs
+++ AbstractMethods/AbstractMethods.cs
@@ -6,5 +6,20 @@ abstract class Shape
 
 }
 
+class Circle : Shape
+{
+    double r;
+    public Circle(double radius) { r = radius; }
+    public override double GetArea() => 3.14 * r * r;
+}
 
 
+class App
+{
+    static void Main()
+    {
+        Shape c = new Circle(5);
+        Console.WriteLine("Circle Area: " + c.GetArea());
+
+    }
+}
